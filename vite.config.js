e0a28986cdab552324406bfc00b622b853b47ac6
tahp <djtahp@gmail.com>
2025-12-26 09:08:46 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/casinobonustracker/',   // 👈 important for GitHub Pages
+  base: '/',   // 👈 important for GitHub Pages
 })
